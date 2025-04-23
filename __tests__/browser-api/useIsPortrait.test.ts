980cd8cbef06c50230b5767d328c5e99620ab9fb
Alessio Frittoli <info@alessiofrittoli.it>
2025-04-23 16:06:42 +0200

    tests: add `useIsPortrait` unit tests

---
+++ __tests__/browser-api/useIsPortrait.test.ts
@@ -0,0 +1,35 @@
+import { renderHook } from '@testing-library/react'
+import { portraitMediaQuery } from '@alessiofrittoli/web-utils/device'
+
+import { useMediaQuery } from '@/browser-api/useMediaQuery'
+import { useIsPortrait } from '@/browser-api/useIsPortrait'
+
+jest.mock( '@/browser-api/useMediaQuery' )
+const mockedUseMediaQuery = useMediaQuery as jest.Mock
+
+
+describe( 'useIsPortrait', () => {
+
+	it( 'returns true when the device is in portrait mode', () => {
+
+		mockedUseMediaQuery.mockReturnValue( true )
+
+		const { result } = renderHook( () => useIsPortrait() )
+
+		expect( result.current ).toBe( true )
+		expect( mockedUseMediaQuery ).toHaveBeenCalledWith( portraitMediaQuery )
+	} )
+
+
+	it( 'returns false when the device is not in portrait mode', () => {
+
+		mockedUseMediaQuery.mockReturnValue( false )
+
+		const { result } = renderHook( () => useIsPortrait() )
+
+		expect( result.current ).toBe( false )
+		expect( mockedUseMediaQuery ).toHaveBeenCalledWith( portraitMediaQuery )
+		
+	} )
+
+} )
