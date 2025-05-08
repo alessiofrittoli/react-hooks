408beb7c3b45f70147be81ee9c44822f67cdd729
Alessio Frittoli <info@alessiofrittoli.it>
2025-05-08 19:22:42 +0200

    docs: minor improvements

--- src/browser-api/storage/useLocalStorage.ts
+++ src/browser-api/storage/useLocalStorage.ts
@@ -3,10 +3,10 @@ import { useStorage } from './useStorage'
 /**
  * useLocalStorage hook.
  * 
- * @param	key				The local storage item key.
- * @param	initialValue	The local storage item initial value.
+ * @param	key		The local storage item key.
+ * @param	initial	The local storage item initial value.
  */
 export const useLocalStorage = <T = string>(
-	key				: string,
-	initialValue?	: T
-) => useStorage( key, initialValue, 'local' )
+	key		: string,
+	initial?: T
+) => useStorage( key, initial, 'local' )
