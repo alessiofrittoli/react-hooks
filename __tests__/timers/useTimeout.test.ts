74bc68884bab4b5cd093d832aa11bce2e5672baf
Alessio Frittoli <info@alessiofrittoli.it>
2025-05-12 11:35:43 +0200

    tests: add `useLightTimeout` unit tests

--- __tests__/timers/useTimeout.test.ts
+++ __tests__/timers/useTimeout.test.ts
@@ -66,7 +66,7 @@ describe( 'useTimeout', () => {
 	} )
 
 
-	it( 'clears the timeout on unmount', () => {
+	it( 'clears the timer on unmount', () => {
 
 		const callback	= jest.fn()
 		const delay		= 1000
